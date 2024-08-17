0b4661621498e3808a4272b679b2c9814a650ffd
Yashwantk75 <yashwantkk4444@gmail.com>
2024-08-17 20:40:09 +0530

    Initial commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -28,7 +28,7 @@ function Card({ movie, isLiked = false }) {
 
     const addToList = async () => {
         try {
-            await axios.post('http://localhost:5000/api/user/add', {
+            await axios.post('https://netflix-clone-ye9l.onrender.com/add', {
                 email,
                 data: movie,
             }).catch((err) => { console.log(err); });
